78e413e6a889891e16873b345769ae8dc696fb0a
nikhil-muskowl <nikhil.muskowl@gmail.com>
2018-11-19 15:21:03 +0530

    dasd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ThankYouComponent } from './pages/thank-you/thank-you.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about-us', component: AboutComponent },
   { path: 'contact-us', component: ContactComponent },
+  { path: 'thank-you', component: ThankYouComponent },
+  { path: 'not-found', component: NotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ThankYouComponent } from './pages/thank-you/thank-you.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    ThankYouComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
